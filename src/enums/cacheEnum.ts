4bc0c983fc47059c9347833b1f93ba61ad3b1f7b
lengguixian <lengguixian@crf-tech.com>
2023-01-04 11:29:15 +0800

    chore: 更改vscode配置项

---
+++ src/enums/cacheEnum.ts
@@ -0,0 +1,10 @@
+// token key
+export const TOKEN_KEY = 'TOKEN__';
+
+export const LOCALE_KEY = 'LOCALE__';
+
+// user info key
+export const USER_INFO_KEY = 'USER__INFO__';
+
+// role info key
+export const ROLES_KEY = 'ROLES__KEY__';
